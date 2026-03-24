2e747edb3a5089c9658335ec8450391f3a74281a
Cat4848 <catalinrol@gmail.com>
2026-03-24 06:30:47 +0000

    feat: built test for when the players names are not valid

--- src/test/java/UserInputTests/StreamUserInputTests.java
+++ src/test/java/UserInputTests/StreamUserInputTests.java
@@ -5,14 +5,12 @@ import UserInput.*;
 import Logger.*;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 
 import java.io.ByteArrayInputStream;
-
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 
 @DisplayName("Test suite for the StreamUserInput")
 public class StreamUserInputTests {
@@ -34,6 +32,21 @@ public class StreamUserInputTests {
     String[] playersNames = userInput.getPlayersName();
     String[] expected = {"Mike", "Frank"};
     assertArrayEquals(expected, playersNames);
+  }
+
+  @Test
+  void ifItThrowsWhenPlayerNameIsEmptyString() {
+    String names = "VeryLongNameOverTwentyCharactersLong\nRalph\nJohn\nLuke";
+    ByteArrayInputStream bais = new ByteArrayInputStream(names.getBytes());
+    ILogger mockedLogger = mock(ConsoleLogger.class);
+    IUserInput userInput = new StreamUserInput(bais, mockedLogger, playerInputValidator);
+    String[] playersNames = userInput.getPlayersName();
+
+    verify(mockedLogger, times(2)).logMessage("Name of Player 1:");
+    verify(mockedLogger, times(1)).logMessage("Name of Player 2:");
+    verify(mockedLogger, times(1)).logMessage("Invalid player name. Let's try again.");
+
+    assertEquals(2, playersNames.length);
   }
 
   @Test
