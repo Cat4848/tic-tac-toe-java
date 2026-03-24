caa4c35687896f3729c0bdc0b20a313c8119e06f
Cat4848 <catalinrol@gmail.com>
2026-03-24 03:20:18 +0000

    test: built test for getting the player move

--- src/test/java/UserInputTests/StreamUserInputTests.java
+++ src/test/java/UserInputTests/StreamUserInputTests.java
@@ -4,6 +4,7 @@ import UserInput.*;
 import Logger.*;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 
@@ -29,5 +30,20 @@ public class StreamUserInputTests {
     String[] playersNames = userInput.getPlayersName();
     String[] expected = {"Mike", "Frank"};
     assertArrayEquals(expected, playersNames);
+  }
+
+  @Test
+  void ifReturnsThePlayerMoveCorrectly() {
+    String moves = "1,1\n2,1";
+    ByteArrayInputStream bais = new ByteArrayInputStream(moves.getBytes());
+    IUserInput userInput = new StreamUserInput(bais, logger);
+
+    int[] playerMove1 = userInput.getPlayerMove();
+    int[] expected1 = new int[]{1, 1};
+    assertArrayEquals(expected1, playerMove1);
+
+    int[] playerMove2 = userInput.getPlayerMove();
+    int[] expected2 = new int[]{2, 1};
+    assertArrayEquals(expected2, playerMove2);
   }
 }
